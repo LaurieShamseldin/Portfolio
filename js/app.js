dc238190989600a1edbbbd090f507aa13d5cd6d2
Laurie Shamseldin <laurie.musson@gmail.com>
2023-02-09 19:31:03 +0100

    feat: Add Project Bloc

--- js/app.js
+++ js/app.js
@@ -18,6 +18,19 @@ const app = {
   const experienceListElms = document.querySelectorAll('.experience-content-name_list .list-element');
   experienceListElms.forEach(item => { item.addEventListener('click', app.displayExperience)});
 
+  const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    centeredSlides: false,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
   }, 
 
